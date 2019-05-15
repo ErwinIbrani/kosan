e96c11844d53fde150589b8e9236d4f09b1e1f39
Erwin <erwin@gti.co.id>
2019-05-15 09:17:27 +0700

    'updateSQLfile'

--- config/kosan.sql
+++ config/kosan.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50721
  File Encoding         : 65001
 
- Date: 07/05/2019 22:41:34
+ Date: 15/05/2019 09:16:34
 */
 
 SET NAMES utf8mb4;
@@ -238,6 +238,40 @@ CREATE TABLE `auth_rule`  (
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
+-- Table structure for cron_job
+-- ----------------------------
+DROP TABLE IF EXISTS `cron_job`;
+CREATE TABLE `cron_job`  (
+  `id_cron_job` int(11) NOT NULL AUTO_INCREMENT,
+  `controller` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `action` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `limit` int(11) NULL DEFAULT NULL,
+  `offset` int(11) NULL DEFAULT NULL,
+  `running` smallint(6) UNSIGNED NOT NULL,
+  `success` smallint(6) UNSIGNED NOT NULL,
+  `started_at` int(11) UNSIGNED NULL DEFAULT NULL,
+  `ended_at` int(11) UNSIGNED NULL DEFAULT NULL,
+  `last_execution_time` float NULL DEFAULT NULL,
+  PRIMARY KEY (`id_cron_job`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 13 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of cron_job
+-- ----------------------------
+INSERT INTO `cron_job` VALUES (1, 'pemberitahuan', 'index', 0, 0, 0, 1, 1557841887, 1557841888, 0.0550029);
+INSERT INTO `cron_job` VALUES (2, 'pemberitahuan', 'index', 0, 0, 0, 1, 1557842068, 1557843847, 0.070004);
+INSERT INTO `cron_job` VALUES (3, 'pemberitahuan', 'index', 0, 0, 0, 1, 1557843890, 1557844121, 0.0510032);
+INSERT INTO `cron_job` VALUES (4, 'pemberitahuan', 'index', 0, 0, 0, 1, 1557844159, 1557844160, 0.0930049);
+INSERT INTO `cron_job` VALUES (5, 'pemberitahuan', 'index', 0, 0, 0, 1, 1557844179, 1557844179, 0.0990059);
+INSERT INTO `cron_job` VALUES (6, 'pemberitahuan', 'index', 0, 0, 0, 1, 1557844212, 1557844212, 0.073004);
+INSERT INTO `cron_job` VALUES (7, 'pemberitahuan', 'index', 0, 0, 0, 1, 1557844230, 1557844536, 0.0800052);
+INSERT INTO `cron_job` VALUES (8, 'pemberitahuan', 'index', 0, 0, 0, 1, 1557844734, 1557845168, 0.0360019);
+INSERT INTO `cron_job` VALUES (9, 'pemberitahuan', 'index', 0, 0, 0, 1, 1557845299, 1557886029, 0.0170012);
+INSERT INTO `cron_job` VALUES (10, 'pemberitahuan', 'index', 0, 0, 0, 1, 1557886092, 1557886095, 3.78422);
+INSERT INTO `cron_job` VALUES (11, 'pemberitahuan', 'index', 0, 0, 0, 1, 1557886290, 1557886293, 2.49914);
+INSERT INTO `cron_job` VALUES (12, 'pemberitahuan', 'index', 0, 0, 0, 1, 1557886441, 1557886444, 3.4442);
+
+-- ----------------------------
 -- Table structure for komplain
 -- ----------------------------
 DROP TABLE IF EXISTS `komplain`;
@@ -270,13 +304,43 @@ CREATE TABLE `kosan`  (
   `status` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT 'Tersedia',
   `gambar_kosan` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 34 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of kosan
 -- ----------------------------
-INSERT INTO `kosan` VALUES (2, 'Kosan Ibu Dia', 34, 100000.00, 'Jl. Raya', 'AC', 'Pria', 'Tersedia', '31fff5a615114521b4fc56d6d04ab056eed513a6.png');
+INSERT INTO `kosan` VALUES (2, 'Kosan Ibu Dia', 33, 100000.00, 'Jl. Raya', 'AC', 'Pria', 'Tersedia', '31fff5a615114521b4fc56d6d04ab056eed513a6.png');
 INSERT INTO `kosan` VALUES (3, 'Kosan Kamu', 13, 130000.00, 'Jl. Dipatikukur', 'AC, WC, WIFI', 'Campur', 'Tersedia', 'ec517688265cf7968bc243e8a154f9f2aab8a862.png');
+INSERT INTO `kosan` VALUES (4, 'Kosan Ibu Dia', 34, 100000.00, 'Jl. Raya', 'AC', 'Pria', 'Tersedia', '31fff5a615114521b4fc56d6d04ab056eed513a6.png');
+INSERT INTO `kosan` VALUES (5, 'Kosan Kamu', 13, 130000.00, 'Jl. Dipatikukur', 'AC, WC, WIFI', 'Campur', 'Tersedia', 'ec517688265cf7968bc243e8a154f9f2aab8a862.png');
+INSERT INTO `kosan` VALUES (6, 'Kosan Ibu Dia', 34, 100000.00, 'Jl. Raya', 'AC', 'Pria', 'Tersedia', '31fff5a615114521b4fc56d6d04ab056eed513a6.png');
+INSERT INTO `kosan` VALUES (7, 'Kosan Kamu', 13, 130000.00, 'Jl. Dipatikukur', 'AC, WC, WIFI', 'Campur', 'Tersedia', 'ec517688265cf7968bc243e8a154f9f2aab8a862.png');
+INSERT INTO `kosan` VALUES (8, 'Kosan Ibu Dia', 34, 100000.00, 'Jl. Raya', 'AC', 'Pria', 'Tersedia', '31fff5a615114521b4fc56d6d04ab056eed513a6.png');
+INSERT INTO `kosan` VALUES (9, 'Kosan Kamu', 13, 130000.00, 'Jl. Dipatikukur', 'AC, WC, WIFI', 'Campur', 'Tersedia', 'ec517688265cf7968bc243e8a154f9f2aab8a862.png');
+INSERT INTO `kosan` VALUES (10, 'Kosan Ibu Dia', 34, 100000.00, 'Jl. Raya', 'AC', 'Pria', 'Tersedia', '31fff5a615114521b4fc56d6d04ab056eed513a6.png');
+INSERT INTO `kosan` VALUES (11, 'Kosan Kamu', 13, 130000.00, 'Jl. Dipatikukur', 'AC, WC, WIFI', 'Campur', 'Tersedia', 'ec517688265cf7968bc243e8a154f9f2aab8a862.png');
+INSERT INTO `kosan` VALUES (12, 'Kosan Ibu Dia', 34, 100000.00, 'Jl. Raya', 'AC', 'Pria', 'Tersedia', '31fff5a615114521b4fc56d6d04ab056eed513a6.png');
+INSERT INTO `kosan` VALUES (13, 'Kosan Kamu', 13, 130000.00, 'Jl. Dipatikukur', 'AC, WC, WIFI', 'Campur', 'Tersedia', 'ec517688265cf7968bc243e8a154f9f2aab8a862.png');
+INSERT INTO `kosan` VALUES (14, 'Kosan Ibu Dia', 34, 100000.00, 'Jl. Raya', 'AC', 'Pria', 'Tersedia', '31fff5a615114521b4fc56d6d04ab056eed513a6.png');
+INSERT INTO `kosan` VALUES (15, 'Kosan Kamu', 13, 130000.00, 'Jl. Dipatikukur', 'AC, WC, WIFI', 'Campur', 'Tersedia', 'ec517688265cf7968bc243e8a154f9f2aab8a862.png');
+INSERT INTO `kosan` VALUES (16, 'Kosan Ibu Dia', 34, 100000.00, 'Jl. Raya', 'AC', 'Pria', 'Tersedia', '31fff5a615114521b4fc56d6d04ab056eed513a6.png');
+INSERT INTO `kosan` VALUES (17, 'Kosan Kamu', 13, 130000.00, 'Jl. Dipatikukur', 'AC, WC, WIFI', 'Campur', 'Tersedia', 'ec517688265cf7968bc243e8a154f9f2aab8a862.png');
+INSERT INTO `kosan` VALUES (18, 'Kosan Ibu Dia', 34, 100000.00, 'Jl. Raya', 'AC', 'Pria', 'Tersedia', '31fff5a615114521b4fc56d6d04ab056eed513a6.png');
+INSERT INTO `kosan` VALUES (19, 'Kosan Kamu', 13, 130000.00, 'Jl. Dipatikukur', 'AC, WC, WIFI', 'Campur', 'Tersedia', 'ec517688265cf7968bc243e8a154f9f2aab8a862.png');
+INSERT INTO `kosan` VALUES (20, 'Kosan Ibu Dia', 34, 100000.00, 'Jl. Raya', 'AC', 'Pria', 'Tersedia', '31fff5a615114521b4fc56d6d04ab056eed513a6.png');
+INSERT INTO `kosan` VALUES (21, 'Kosan Kamu', 13, 130000.00, 'Jl. Dipatikukur', 'AC, WC, WIFI', 'Campur', 'Tersedia', 'ec517688265cf7968bc243e8a154f9f2aab8a862.png');
+INSERT INTO `kosan` VALUES (22, 'Kosan Ibu Dia', 34, 100000.00, 'Jl. Raya', 'AC', 'Pria', 'Tersedia', '31fff5a615114521b4fc56d6d04ab056eed513a6.png');
+INSERT INTO `kosan` VALUES (23, 'Kosan Kamu', 13, 130000.00, 'Jl. Dipatikukur', 'AC, WC, WIFI', 'Campur', 'Tersedia', 'ec517688265cf7968bc243e8a154f9f2aab8a862.png');
+INSERT INTO `kosan` VALUES (24, 'Kosan Ibu Dia', 34, 100000.00, 'Jl. Raya', 'AC', 'Pria', 'Tersedia', '31fff5a615114521b4fc56d6d04ab056eed513a6.png');
+INSERT INTO `kosan` VALUES (25, 'Kosan Kamu', 13, 130000.00, 'Jl. Dipatikukur', 'AC, WC, WIFI', 'Campur', 'Tersedia', 'ec517688265cf7968bc243e8a154f9f2aab8a862.png');
+INSERT INTO `kosan` VALUES (26, 'Kosan Ibu Dia', 34, 100000.00, 'Jl. Raya', 'AC', 'Pria', 'Tersedia', '31fff5a615114521b4fc56d6d04ab056eed513a6.png');
+INSERT INTO `kosan` VALUES (27, 'Kosan Kamu', 13, 130000.00, 'Jl. Dipatikukur', 'AC, WC, WIFI', 'Campur', 'Tersedia', 'ec517688265cf7968bc243e8a154f9f2aab8a862.png');
+INSERT INTO `kosan` VALUES (28, 'Kosan Ibu Dia', 34, 100000.00, 'Jl. Raya', 'AC', 'Pria', 'Tersedia', '31fff5a615114521b4fc56d6d04ab056eed513a6.png');
+INSERT INTO `kosan` VALUES (29, 'Kosan Kamu', 13, 130000.00, 'Jl. Dipatikukur', 'AC, WC, WIFI', 'Campur', 'Tersedia', 'ec517688265cf7968bc243e8a154f9f2aab8a862.png');
+INSERT INTO `kosan` VALUES (30, 'Kosan Ibu Dia', 34, 100000.00, 'Jl. Raya', 'AC', 'Pria', 'Tersedia', '31fff5a615114521b4fc56d6d04ab056eed513a6.png');
+INSERT INTO `kosan` VALUES (31, 'Kosan Kamu', 13, 130000.00, 'Jl. Dipatikukur', 'AC, WC, WIFI', 'Campur', 'Tersedia', 'ec517688265cf7968bc243e8a154f9f2aab8a862.png');
+INSERT INTO `kosan` VALUES (32, 'Kosan Ibu Dia', 34, 100000.00, 'Jl. Raya', 'AC', 'Pria', 'Tersedia', '31fff5a615114521b4fc56d6d04ab056eed513a6.png');
+INSERT INTO `kosan` VALUES (33, 'Kosan Kamu', 13, 130000.00, 'Jl. Dipatikukur', 'AC, WC, WIFI', 'Campur', 'Tersedia', 'ec517688265cf7968bc243e8a154f9f2aab8a862.png');
 
 -- ----------------------------
 -- Table structure for migration
@@ -293,6 +357,7 @@ CREATE TABLE `migration`  (
 -- ----------------------------
 INSERT INTO `migration` VALUES ('m000000_000000_base', 1556848765);
 INSERT INTO `migration` VALUES ('m140506_102106_rbac_init', 1556848773);
+INSERT INTO `migration` VALUES ('m150927_060316_cronjob_init', 1557841264);
 INSERT INTO `migration` VALUES ('m170907_052038_rbac_add_index_on_auth_assignment_user_id', 1556848773);
 INSERT INTO `migration` VALUES ('m180523_151638_rbac_updates_indexes_without_prefix', 1556848774);
 
@@ -315,13 +380,15 @@ CREATE TABLE `user`  (
   `alamat` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `poto_ktp` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL,
   `tanggal_daftar` datetime(0) NOT NULL,
+  `status_kost` smallint(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES (8, 'Santi', 'erwin', 'Perempuan', '2019-05-01', 'Bandung', '098564833844', 'WctfhkEgrJD4QFFi_1rJcNTrhQqHHBJf', '$2y$13$KleWkwitzS0x3Px7zUMyReeMaoHZH9g8I/MpQ8ttCxchPaOq8t/Be', 'santi@gmail.com', 10, 'Jl. ajaa', 'aab0504a2939cc1cbd2e79ba6673d305568854bc.png', '2019-05-06 09:19:30');
+INSERT INTO `user` VALUES (8, 'Santi', 'erwin', 'Perempuan', '2019-05-01', 'Bandung', '098564833844', 'WctfhkEgrJD4QFFi_1rJcNTrhQqHHBJf', '$2y$13$KleWkwitzS0x3Px7zUMyReeMaoHZH9g8I/MpQ8ttCxchPaOq8t/Be', 'santi@gmail.com', 10, 'Jl. ajaa', 'aab0504a2939cc1cbd2e79ba6673d305568854bc.png', '2019-05-06 09:19:30', 0);
+INSERT INTO `user` VALUES (9, 'Erwin', 'Wiguna', 'Laki-laki', '2019-05-12', 'Jakarta', '085351644655', 'RQVWrKgjWKnhf7StRPGc3dZSuTz8oTEr', '$2y$13$vSK84.SI13Hw0ZKrmmk.AuWWPkXJHrYmPVBSvxWnnMgVy1wqJQAci', 'erwinlocalhost90@gmail.com', 10, 'Jl. Raya', 'cc49921c9b399902164ae00d9757b6faa246a8f7.png', '2019-05-13 13:54:15', 1);
 
 -- ----------------------------
 -- Table structure for user_kosan
@@ -334,13 +401,22 @@ CREATE TABLE `user_kosan`  (
   `tgl_masuk_kos` date NOT NULL,
   `tgl_berakhir_kos` date NOT NULL,
   `status` enum('Tetap','Keluar') CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT 'Tetap',
-  `status_bayar` enum('Dibayar','Belum Dibayar') CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT 'Dibayar',
-  `pemebritahuan` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `status_konfirmasi` enum('Dikonfirmasi','Belum Dikonfirmasi') CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT 'Belum Dikonfirmasi',
+  `status_bayar` enum('Dibayar','Belum Dibayar') CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT 'Belum Dibayar',
+  `periode_kosan` int(11) NOT NULL DEFAULT 1,
+  `bukti_pembayaran` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL,
+  `status_cron_job` enum('Dieksekusi','Belum Dieksekusi') CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT 'Dieksekusi',
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `user`(`user_id`) USING BTREE,
   INDEX `kosan`(`kosan_id`) USING BTREE,
   CONSTRAINT `kosan` FOREIGN KEY (`kosan_id`) REFERENCES `kosan` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user_kosan
+-- ----------------------------
+INSERT INTO `user_kosan` VALUES (1, 9, 2, '2019-05-13', '2019-06-13', 'Tetap', 'Belum Dikonfirmasi', 'Dibayar', 1, '480924079d00aba1e1865fc373af476e0f75e209.png', 'Dieksekusi');
+INSERT INTO `user_kosan` VALUES (3, 9, 2, '2019-06-13', '2019-05-15', 'Tetap', 'Belum Dikonfirmasi', 'Belum Dibayar', 2, NULL, 'Dieksekusi');
 
 SET FOREIGN_KEY_CHECKS = 1;
